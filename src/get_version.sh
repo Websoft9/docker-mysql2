980cd967ca77fafb9efaeb29c365820ca3a9b238
qiaofeng1227 <76487013@qq.com>
2022-07-26 15:40:11 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,2 @@
-sudo echo MySQL Version:$(docker exec -it $1 mysql -V | awk "{print \$5}" | awk -F"-" "{print \$1}" ) 1>> /data/logs/install_version.txt
+# | awk "{print \$5}" | awk -F"-" "{print \$1}"
+sudo echo MySQL Version:$(docker exec -it $1 mysql -V ) 1>> /data/logs/install_version.txt
